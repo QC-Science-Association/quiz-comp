9795169374242e7d30973e5d33fb2aa67ee268b6
Dys <112629719+QC-Science-Association@users.noreply.github.com>
2023-01-26 00:19:14 +0800

    gh pages

--- src/types/objects.ts
+++
@@ -1,5 +0,0 @@
-export type key = string | number;
-
-export type looseObject = Record<key, unknown> | unknown[];
-
-export type primitive = boolean | number | string;
--- src/utils/extensions.ts
+++
@@ -1,3 +0,0 @@
-export function iota(from: number, to: number) {
-	return Array<number>(to + 1 - from).fill(from).map((v, i) => v + i);
-}
